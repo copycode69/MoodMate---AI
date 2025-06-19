41c1de7b821bbd87f05b529cd4808dd41afd8f4e
copycode69 <jaytiwari092@gmail.com>
2025-06-19 13:17:42 +0530

    udpate

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from predict import predict_sentiment
 from response_links import get_negative_response_link
+import os
 
 app = Flask(__name__)
 
@@ -18,5 +19,7 @@ def home():
             message = "😊 That sounds positive!"
     return render_template("index.html", message=message, link=link)
 
+# 👇 Add this block at the bottom
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # use Render-assigned port
+    app.run(host="0.0.0.0", port=port)
